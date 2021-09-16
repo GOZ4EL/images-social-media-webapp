3a1a9389f61381cfd9a9f2967789d8a7b5dfc423
goz4el <hordemzerado@gmail.com>
2021-09-15 22:57:24 -0400

    Fixin typo in LoginForm's password attribute

--- account/forms.py
+++ account/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField()
-    password = forms.Charfield(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput)
