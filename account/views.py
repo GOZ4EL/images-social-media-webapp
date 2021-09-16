a6630ec47233bece27519bb93e7b42f5f2dee241
goz4el <hordemzerado@gmail.com>
2021-09-15 23:06:42 -0400

    Fixing typo in user_login

--- account/views.py
+++ account/views.py
@@ -8,7 +8,7 @@ from .forms import LoginForm
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             cd = form.cleaned_data
             user = authenticate(request,
                                 username=cd['username'],
