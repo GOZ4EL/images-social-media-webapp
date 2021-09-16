6b68de9257ec6135cd37b926767c12e8f527ef0f
goz4el <hordemzerado@gmail.com>
2021-09-15 23:15:04 -0400

    Fixing error in what does login returns

--- account/views.py
+++ account/views.py
@@ -22,6 +22,6 @@ def user_login(request):
                     return HttpResponse('Disabled account')
             else:
                 return HttpResponse('Invalid login')
-        else:
-            form = LoginForm()
-        return render(request, 'account/login.html', {'form': form})
+    else:
+        form = LoginForm()
+    return render(request, 'account/login.html', {'form': form})
