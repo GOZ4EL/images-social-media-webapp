059a4f1163a623dc3eef61acd54e9acd92a9b172
goz4el <hordemzerado@gmail.com>
2021-09-25 10:27:31 -0400

    Adding dashboard's view URL pattern

--- account/urls.py
+++ account/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard'),
 ]
