8d9dcaf07e2b81c2f495908fc6069594183d0066
goz4el <hordemzerado@gmail.com>
2021-09-25 10:58:46 -0400

    Adding password_change and password_change_done to the url list

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,11 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
+    # Change password urls
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(),
+         name='password_change'),
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(),
+         name='password_change_done'),
 ]
