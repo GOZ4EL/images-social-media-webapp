35b9569772671da9d84a89026f7f7a1b147e48f0
goz4el <hordemzerado@gmail.com>
2021-09-27 03:14:54 -0400

    Adding register path to urls

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
