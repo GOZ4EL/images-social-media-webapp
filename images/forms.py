f6a85d8f199ad83b4c12fb7df65308e6c93405d0
goz4el <hordemzerado@gmail.com>
2021-09-28 16:11:32 -0400

    Adding basic validation for checking if the url is a valid jpeg image

--- images/forms.py
+++ images/forms.py
@@ -4,6 +4,15 @@ from .models import Image
 
 
 class ImageCreateForm(forms.ModelForm):
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg', 'jpeg']
+        extension = url.rsplit('.', 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The given url does not ' \
+                                        'match valid image extensions.')
+        return url
+
     class Meta:
         model = Image
         fields = ('title', 'url', 'description')
