0e0b36732c962fd5be84e44f0c83130112c5ddb3
goz4el <hordemzerado@gmail.com>
2021-09-28 11:06:04 -0400

    Adding media configuration

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -128,3 +128,9 @@ LOGOUT_URL = 'logout'
 # Email
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+# Media
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
