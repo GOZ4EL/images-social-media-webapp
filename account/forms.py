d94f10815e054b10d974bc103e3b8b20e73c9124
goz4el <hordemzerado@gmail.com>
2021-09-28 11:28:28 -0400

    Creating user and profile edit forms

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
 
+from .models import Profile
+
+
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
@@ -21,3 +24,15 @@ class UserREgistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
+
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('date_of_birth', 'photo')
