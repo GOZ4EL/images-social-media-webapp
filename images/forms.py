821092201589d0215b5acd7169b23cd3fc095e54
goz4el <hordemzerado@gmail.com>
2021-09-28 16:32:31 -0400

    Overriding save method of the Image ModelForm, so it automatically downloads the image of the url given by the user

--- images/forms.py
+++ images/forms.py
@@ -1,3 +1,6 @@
+from urllib import request
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 from django import forms
 
 from .models import Image
@@ -12,6 +15,24 @@ class ImageCreateForm(forms.ModelForm):
             raise forms.ValidationError('The given url does not ' \
                                         'match valid image extensions.')
         return url
+
+    def save(self, force_insert=False,
+                   force_update=False,
+                   commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+
+        # download image from the given URL
+        response = request.urlopen(image_url)
+        image.image.save(image_name,
+                         ContentFile(response.read()),
+                         save=False)
+        if commit:
+            image.save()
+        return imae
 
     class Meta:
         model = Image
