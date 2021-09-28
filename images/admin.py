6be7ef73f79665cf39eafa40ea866e8f89a5ef22
goz4el <hordemzerado@gmail.com>
2021-09-28 15:46:58 -0400

    Adding Image model to the admin sites

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Image
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'image', 'created']
+    list_filter = ['created']
