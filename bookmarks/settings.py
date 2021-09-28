01d77bb526d7c9faba58319848ccd8e4a214bcf8
goz4el <hordemzerado@gmail.com>
2021-09-28 11:18:35 -0400

    Making migration [Create model Profile]

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -1,8 +1,10 @@
 """
 Django settings for bookmarks project.
 """
+import os
 
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
