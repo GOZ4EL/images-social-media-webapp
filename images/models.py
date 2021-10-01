ef9ca2ebd9c0109b479334c5ad14e912ea469afe
goz4el <hordemzerado@gmail.com>
2021-10-01 14:06:49 -0400

    Adding the get_absolute_url() method to the Image model

--- images/models.py
+++ images/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
+from django.urls import reverse
 
 
 class Image(models.Model):
@@ -18,6 +19,9 @@ class Image(models.Model):
     users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
                                         related_name='images_liked',
                                         blank=True)
+
+    def get_absolute_url(self):
+        return reverse('images:detail', args=[self.id, self.slug])
 
     def __str__(self):
         return self.title
