452e13b0c40861f8d816cd4fcfdd7bd3164a3e81
goz4el <hordemzerado@gmail.com>
2021-10-01 14:02:43 -0400

    Adding the image detail view and url pattern

--- images/urls.py
+++ images/urls.py
@@ -8,4 +8,6 @@ app_name = 'images'
 
 urlpatterns = [
     path('create/', views.image_create, name='create'),
+    path('detail/<int:id>/<slug:slug>/',
+         views.image_detail, name='detail'),
 ]
--- images/views.py
+++ images/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect 
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
+from .models import Image
 from .forms import ImageCreateForm
 
 
@@ -27,3 +29,11 @@ def image_create(request):
                   'images/image/create.html',
                   {'section': 'images',
                    'form': form})
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+                  'images/image/detail.html',
+                  {'section': 'images',
+                   'image': image})
