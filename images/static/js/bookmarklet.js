c6479eefc1f104fdfa3e216fd709d12b778c1a2d
goz4el <hordemzerado@gmail.com>
2021-10-01 13:43:03 -0400

    Writting the bookmarklet function code

--- images/static/js/bookmarklet.js
+++ images/static/js/bookmarklet.js
@@ -5,7 +5,63 @@
   let min_width = 100;
   let min_height = 100;
 
-  function bookmarklet(msg) {}
+  function bookmarklet(msg) {
+    // load CSS
+    let css = jQuery("<link>");
+    css.attr({
+      rel: "stylesheet",
+      type: "text/css",
+      href:
+        static_url +
+        "css/bookmarklet.css?r=" +
+        Math.floor(Math.random() * 99999999999999999999),
+    });
+    jQuery("head").append("css");
+
+    // Load HTML
+    box_html = `
+		  <div id="bookmarklet">
+		    <a href="#" id="close">&times;</a>
+			<h1>Select an image to bookmark:</h1>
+			<div class="images"></div>
+		  </div>
+		`;
+    jQuery("body").append(box_html);
+
+    // Close event
+    jQuery("#bookmarklet #close").click(function () {
+      jQuery("#bookmarklet").remove();
+    });
+
+    // Find images and display them
+    jQuery.each(jQuery('img[src$="jpg"]'), function (index, image) {
+      if (
+        jQuery(image).width() >= min_width &&
+        jQuery(image).height >= min_height
+      ) {
+        image_url = jQuery(image).attr("src");
+        jQuery("#bookmarklet .images").append(
+          '<a href="#"><img src="' + image_url + '"></a>'
+        );
+      }
+    });
+  }
+
+  // When an image is selected open URL with it
+  jQuery("#bookmarklet .images a").click(function (e) {
+    selected_image = jQuery(this).children("img").attr("src");
+    // Hide bookmarklet
+    jQuery("#bookmarklet").hide();
+    // Open new window to submit the image
+    window.open(
+      site_url +
+        "images/create/?url=" +
+        encodeURIComponent(selected_image) +
+        "&title=" +
+        encodeURIComponent(jQuery("title").text()),
+      "_blank"
+    );
+  });
 
   // Check if jquery is loaded
   if (typeof window.jQuery != "undefined") {
