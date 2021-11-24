08ff5322444008df8ecdeffae7b2a9f571df52aa
goz4el <hordemzerado@gmail.com>
2021-11-23 22:10:07 -0400

    Adding image_list to the images urls

--- images/urls.py
+++ images/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("create/", views.image_create, name="create"),
     path("detail/<int:id>/<slug:slug>/", views.image_detail, name="detail"),
     path("like/", views.image_like, name="like"),
+    path("", views.image_list, name="list"),
 ]
