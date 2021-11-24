8ad2855e100d848d1d4c2655519594d6a8bcaadf
goz4el <hordemzerado@gmail.com>
2021-11-24 13:17:03 -0400

    Adding Action model

--- actions/models.py
+++ actions/models.py
@@ -1,3 +1,17 @@
+"""Models of the actions app"""
 from django.db import models
 
-# Create your models here.
+
+class Action(models.Model):
+    """
+    This is where actions performed by the users are stored
+    """
+
+    user = models.ForeignKey(
+        "auth.User", related_name="actions", db_index=True, on_delete=models.CASCADE
+    )
+    verb = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ("-created",)
