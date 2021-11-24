1271a04bb54641fa7a194fc980532cfb7f5054f1
goz4el <hordemzerado@gmail.com>
2021-11-24 10:32:02 -0400

    Creating list and detail views for User

--- account/views.py
+++ account/views.py
@@ -1,87 +1,90 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 from .models import Profile
-from .forms import (
-    LoginForm,
-    UserRegistrationForm,
-    UserEditForm,
-    ProfileEditForm
-)
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 
 
 def register(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
             new_user = user_form.save(commit=False)
-            new_user.set_password(
-                user_form.cleaned_data['password'])
+            new_user.set_password(user_form.cleaned_data["password"])
             new_user.save()
-            
+
             Profile.objects.create(user=new_user)
 
-            return render(request,
-                          'account/register_done.html',
-                          {'new_user': new_user})
+            return render(request, "account/register_done.html", {"new_user": new_user})
     else:
         user_form = UserRegistrationForm()
-    return render(request,
-                  'account/register.html',
-                  {'user_form': user_form})
+    return render(request, "account/register.html", {"user_form": user_form})
 
 
 def user_login(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(request,
-                                username=cd['username'],
-                                password=cd['password'])
+            user = authenticate(
+                request, username=cd["username"], password=cd["password"]
+            )
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Authenticated '\
-                                        'Successfully')
+                    return HttpResponse("Authenticated " "Successfully")
                 else:
-                    return HttpResponse('Disabled account')
+                    return HttpResponse("Disabled account")
             else:
-                return HttpResponse('Invalid login')
+                return HttpResponse("Invalid login")
     else:
         form = LoginForm()
-    return render(request, 'account/login.html', {'form': form})
+    return render(request, "account/login.html", {"form": form})
 
 
 @login_required
 def dashboard(request):
-    return render(request,
-                  'account/dashboard.html',
-                  {'section': 'dashboard'})
+    return render(request, "account/dashboard.html", {"section": "dashboard"})
 
 
 @login_required
 def edit(request):
-    if request.method == 'POST':
-        user_form = UserEditForm(instance=request.user,
-                                 data=request.POST)
-        profile_form = ProfileEditForm(instance=request.user.profile,
-                                       data=request.POST,
-                                       files=request.FILES)
+    if request.method == "POST":
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile, data=request.POST, files=request.FILES
+        )
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            messages.success(request,
-                             'Profile updated successfully')
+            messages.success(request, "Profile updated successfully")
         else:
-            messages.error(request, 'Error updating your profile')
+            messages.error(request, "Error updating your profile")
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
-    return render(request,
-                  'account/edit.html',
-                  {'user_form': user_form,
-                   'profile_form': profile_form})
+    return render(
+        request,
+        "account/edit.html",
+        {"user_form": user_form, "profile_form": profile_form},
+    )
+
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(
+        request, "account/user/list.html", {"section": "people", "users": users}
+    )
+
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User, username=username, is_active=True)
+    return render(
+        request, "account/user/detail.html", {"section": "people", "user": user}
+    )
