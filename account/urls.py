57eec79e00f10e67915147a5179fa33f439bb390
goz4el <hordemzerado@gmail.com>
2021-11-24 12:52:41 -0400

    Changing the order of the user_follow vie

--- account/urls.py
+++ account/urls.py
@@ -39,6 +39,6 @@ urlpatterns = [
         name="password_reset_complete",
     ),
     path("users/", views.user_list, name="user_list"),
-    path("users/<username>/", views.user_detail, name="user_detail"),
     path("users/follow/", views.user_follow, name="user_follow"),
+    path("users/<username>/", views.user_detail, name="user_detail"),
 ]
