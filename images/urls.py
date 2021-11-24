5cce3ec4770f1949c4bab3f2115bc120a21da35b
goz4el <hordemzerado@gmail.com>
2021-11-23 20:56:32 -0400

    Adding like view to the image route list

--- images/urls.py
+++ images/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 
-app_name = 'images'
+app_name = "images"
 
 
 urlpatterns = [
-    path('create/', views.image_create, name='create'),
-    path('detail/<int:id>/<slug:slug>/',
-         views.image_detail, name='detail'),
+    path("create/", views.image_create, name="create"),
+    path("detail/<int:id>/<slug:slug>/", views.image_detail, name="detail"),
+    path("like/", views.image_like, name="like"),
 ]
