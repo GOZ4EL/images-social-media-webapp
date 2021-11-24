64cef3b941dba4aaf1b15567c26c82a96119bcd7
goz4el <hordemzerado@gmail.com>
2021-11-24 14:23:35 -0400

    Registering the Action model in the administration site

--- actions/admin.py
+++ actions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Action
+
+
+@admin.register(Action)
+class ActionAdmin(admin.ModelAdmin):
+    list_display = ("user", "verb", "target", "created")
+    list_filter = ("created",)
+    search_fields = ("verb",)
