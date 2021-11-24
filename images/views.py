dd44248d04066ebc67acf2845a50d0c7a940e5dc
goz4el <hordemzerado@gmail.com>
2021-11-24 14:46:12 -0400

    Adding action creation to images views

--- images/views.py
+++ images/views.py
@@ -7,6 +7,7 @@ from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from common.decorators import ajax_required
+from actions.utils import create_action
 from .models import Image
 from .forms import ImageCreateForm
 
@@ -22,6 +23,7 @@ def image_create(request):
             new_item.user = request.user
 
             new_item.save()
+            create_action(request.user, "bookmarked image", new.item)
             messages.success(request, "Image added successfully")
 
             return redirect(new_item.get_absolute_url())
@@ -52,6 +54,7 @@ def image_like(request):
             image = Image.objects.get(id=image_id)
             if action == "like":
                 image.users.like_add(request.user)
+                create_action(request.user, "likes", image)
             else:
                 image.users_like.remove(request.user)
             return JsonResponse({"status": "ok"})
