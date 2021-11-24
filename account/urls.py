aa59a7c25b215d6efb30523438ccc2c21d952c92
goz4el <hordemzerado@gmail.com>
2021-11-24 12:27:19 -0400

    Adding the user_follow view to the account app urls

--- account/urls.py
+++ account/urls.py
@@ -40,4 +40,5 @@ urlpatterns = [
     ),
     path("users/", views.user_list, name="user_list"),
     path("users/<username>/", views.user_detail, name="user_detail"),
+    path("users/follow/", views.user_follow, name="user_follow"),
 ]
