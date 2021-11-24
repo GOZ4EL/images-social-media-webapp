e3d7415c1bf0ed4e8a02f7075a64fb1cff6ea19b
goz4el <hordemzerado@gmail.com>
2021-11-24 10:21:51 -0400

    Building Contact model and adding it as a many-to-many self relationship on the User model

--- account/models.py
+++ account/models.py
@@ -1,13 +1,46 @@
+"""Models of the account App"""
 from django.db import models
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL,
-                                on_delete=models.CASCADE)
+    """Profile class. It has a auth.User and a birthdate and a photo"""
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d',
-                              blank=True)
+    photo = models.ImageField(upload_to="users/%Y/%m/%d", blank=True)
 
     def __str__(self):
-        return f'Profile for user {self.user.username}'
+        return f"Profile for user {self.user.username}"
+
+
+class Contact(models.Model):
+    """
+    Model for making a many-to-many Users relationship.
+    Making it possible to have a follow system
+    """
+
+    user_from = models.ForeignKey(
+        "auth.User", related_name="rel_from_set", on_delete=models.CASCADE
+    )
+    user_to = models.ForeignKey(
+        "auth.User", related_name="rel_to_set", on_delete=models.CASCADE
+    )
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self):
+        return f"{self.user_from} follows {self.user_to}"
+
+
+# Add following field to User dynamically
+user_model = get_user_model()
+user_model.add_to_class(
+    "following",
+    models.ManyToManyField(
+        "self", through=Contact, related_name="followers", symmetrical=False
+    ),
+)
