a2eb9f4b9d821ea31f5489c86afe8d4612591664
goz4el <hordemzerado@gmail.com>
2021-11-25 16:09:13 -0400

    Adding images signals to be initializedt when the images app is launched

--- images/apps.py
+++ images/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class ImagesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'images'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "images"
+
+    def ready(self):
+        import images.signals
