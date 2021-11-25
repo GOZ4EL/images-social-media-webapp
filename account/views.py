a794529edf4707200952f49f020ebe228ad3197f
goz4el <hordemzerado@gmail.com>
2021-11-25 14:19:23 -0400

    Adding to the dashboard view the last 10 actions performed by users the one logged in follows

--- account/views.py
+++ account/views.py
@@ -8,6 +8,7 @@ from django.views.decorators.http import require_POST
 
 from common.decorators import ajax_required
 from actions.utils import create_action
+from actions.models import Action
 from .models import Profile, Contact
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 
@@ -52,7 +53,22 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
-    return render(request, "account/dashboard.html", {"section": "dashboard"})
+    """
+    View for rendering the dashboard
+    """
+
+    actions = Action.objects.exclude(user=request.user)
+    following_ids = request.user.following.values_list("id", flat=True)
+    if following_ids:
+        actions = actions.filter(user_id__in=following_ids)
+    actions = actions[:10]
+
+    return render(
+        request,
+        "account/dashboard.html",
+        {"section": "dashboard"},
+        {"actions": actions},
+    )
 
 
 @login_required
