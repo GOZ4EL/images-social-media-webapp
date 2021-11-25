b234093c47a9456a67be6d14ce2f943d1a8830d8
goz4el <hordemzerado@gmail.com>
2021-11-25 16:37:56 -0400

    Adding redis configuration

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -93,6 +93,9 @@ DATABASES = {
     }
 }
 
+REDIS_HOST = "localhost"
+REDIS_PORT = 6379
+REDIS_DB = 0
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
