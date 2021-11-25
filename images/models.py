deaf4e15e939004b10e455cb05ee47af3af8e021
goz4el <hordemzerado@gmail.com>
2021-11-25 15:55:48 -0400

    Adding total_likes field to Image model

--- images/models.py
+++ images/models.py
@@ -5,23 +5,24 @@ from django.urls import reverse
 
 
 class Image(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                             related_name='images_created',
-                             on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="images_created",
+        on_delete=models.CASCADE,
+    )
     title = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200,
-                            blank=True)
+    slug = models.SlugField(max_length=200, blank=True)
     url = models.URLField()
-    image = models.ImageField(upload_to='images/%Y/%m/%d/')
+    image = models.ImageField(upload_to="images/%Y/%m/%d/")
     description = models.TextField(blank=True)
-    created = models.DateField(auto_now_add=True,
-                               db_index=True)
-    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
-                                        related_name='images_liked',
-                                        blank=True)
+    created = models.DateField(auto_now_add=True, db_index=True)
+    users_like = models.ManyToManyField(
+        settings.AUTH_USER_MODEL, related_name="images_liked", blank=True
+    )
+    total_likes = models.PositiveIntegerField(db_index=True, default=0)
 
     def get_absolute_url(self):
-        return reverse('images:detail', args=[self.id, self.slug])
+        return reverse("images:detail", args=[self.id, self.slug])
 
     def __str__(self):
         return self.title
